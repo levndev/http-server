e32321cdb8bfeb14eb8ee51d6a0130b9dda1bc31
Lev <levndev@gmail.com>
2022-12-21 01:54:05 +0500

    Mental crisis

--- main.cpp
+++ main.cpp
@@ -42,17 +42,6 @@ void * WorkConnection(void * data) {
     char buf[10240] = {0};
     int flags = 0;
     std::regex rgx(R"((\w+)\s+(\/[^\s\?]*)\S*\s+HTTP\/[\d.]+)");
-    if (test) {
-        std::string response;
-        response.append("HTTP/1.0 404 NOT FOUND\r\n") ;
-        response.append("Content-Length: 0\r\n") ;
-        response.append("Content-Type: text/html\r\n\r\n");
-        //response.append("404");
-        //std::cout << response << std::endl;
-        send(sfd, response.c_str(), response.size(), 0);
-        close(sfd);
-        return NULL;
-    }
     while((length = recv(sfd, &buf, sizeof(buf), flags)) > 0) {
         std::string msg(buf);
         memset(&buf, 0, sizeof(buf));
@@ -148,6 +137,17 @@ int main(int argc, char ** argv) {
         if (connection != -1) {
             //std::thread worker(WorkConnection, connection, directory, test);
             //worker.detach();
+            if (test) {
+                std::string response;
+                response.append("HTTP/1.0 404 NOT FOUND\r\n") ;
+                response.append("Content-Length: 0\r\n") ;
+                response.append("Content-Type: text/html\r\n\r\n");
+                //response.append("404");
+                //std::cout << response << std::endl;
+                send(connection, response.c_str(), response.size(), 0);
+                close(connection);
+                continue;
+            }
             pthread_t slavethread;
             int pthreadCheck = pthread_create(&slavethread, NULL, WorkConnection, &connection);
             if(pthreadCheck != 0)
