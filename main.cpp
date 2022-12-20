106c0bc51d9527b8098effc5de893db8f66ce395
Lev <levndev@gmail.com>
2022-12-21 04:02:30 +0500

    Mental crisis

--- main.cpp
+++ main.cpp
@@ -16,20 +16,20 @@
 #include <thread>
 
 std::string ReadFile(std::string path, bool * success) {
-    std::fstream input;
-    input.open(path, std::fstream::in | std::fstream::out);
-    if (input.is_open())
+    std::fstream file;
+    file.open(path, std::fstream::in);
+    if (file.is_open())
         *success = true;
     else
         *success = false;
     std::stringstream ssBuffer;
-    ssBuffer << input.rdbuf();
+    ssBuffer << file.rdbuf();
     std::string content = ssBuffer.str();
     return content;
 }
 bool test = false;
 std::string working_directory;
-void SendMsg(int sfd, std::string msg) {
+void Send200(int sfd, std::string msg) {
     std::string response;
     response.append("HTTP/1.0 200 OK\r\n");
     response.append("Content-length: ");
@@ -38,7 +38,6 @@ void SendMsg(int sfd, std::string msg) {
     response.append("Content-Type: text/html\r\n\r\n");
     response.append(msg);
     response.append("\r\n\r\n");
-    //std::cout << response << std::endl;
     send(sfd, response.c_str(), response.size(), 0);
     close(sfd);
 }
@@ -47,73 +46,40 @@ void Send404(int sfd) {
     response.append("HTTP/1.0 404 NOT FOUND\r\n") ;
     response.append("Content-Length: 0\r\n") ;
     response.append("Content-Type: text/html\r\n\r\n");
-    //response.append("404");
-    //std::cout << response << std::endl;
     send(sfd, response.c_str(), response.size(), 0);
     close(sfd);
 }
-void * WorkConnection(void * data) {
-    int sfd = *(int*)data;
+void WorkConnection(int sfd) {
     int flags = 0;
     try {
-        std::regex rgx(R"((\w+)\s+(\/[^\s\?]*)\S*\s+HTTP\/[\d.]+)");
         std::vector<char> buffer(10000);
-        std::string rcv;
+        std::string msg;
         int received;
         do
         {
             received = recv(sfd, &buffer[0], buffer.size(), 0);
-            if(received == -1)
-            {
-                //std::cout << "0 bytes received (error)" << std::endl;
-            }
-            else
-            {
-                rcv.append(buffer.cbegin(), buffer.cend());
-            }
-
+            if(received != -1)
+                msg.append(buffer.cbegin(), buffer.cend());
         } while(received == buffer.size());
-        std::smatch match;
-        if (std::regex_search(rcv, match, rgx)) {
-            if (test) {
-                Send404(sfd);
-                return NULL;
-            }
-            std::string type = match[1];
-            std::string resource = match[2];
-            if (type == "GET") {
-                //std::cout << type << " - " << resource << std::endl;
-                int file_length;
-                bool success = false;
-                std::string file_text = ReadFile(working_directory + resource, &success);
-                if (success) {
-                    std::string response;
-                    response.append("HTTP/1.0 200 OK\r\n");
-                    response.append("Content-length: ");
-                    response.append(std::to_string(file_text.size()));
-                    response.append("\r\n");
-                    response.append("Content-Type: text/html\r\n\r\n");
-                    response.append(file_text);
-                    //response.append("\r\n\r\n");
-                    //std::cout << response << std::endl;
-                    send(sfd, response.c_str(), response.size(), 0);
-                }
-                else {
-                    std::string response;
-                    response.append("HTTP/1.0 404 NOT FOUND\r\n") ;
-                    response.append("Content-Length: 0\r\n") ;
-                    response.append("Content-Type: text/html\r\n\r\n");
-                    //response.append("404");
-                    //std::cout << response << std::endl;
-                    send(sfd, response.c_str(), response.size(), 0);
-                }
-            }
+        size_t left = msg.find_first_of(' ') + 1;
+        size_t right = msg.find_first_of(" ?", left);
+        std::string path = msg.substr(left, right - left);
+        if (test) {
+            Send404(sfd);
+            return;
         }
-    } catch(const std::regex_error& ex) {
-        SendMsg(sfd, ex.what());
+        int file_length;
+        bool success = false;
+        std::string file_text = ReadFile(working_directory + path, &success);
+        if (success) {
+            Send200(sfd, file_text);
+        }
+        else {
+            Send404(sfd);
+        }
+    } catch(const std::exception& ex) {
+        Send200(sfd, ex.what());
     }
-    close(sfd);
-    return NULL;
 }
 
 int main(int argc, char ** argv) {
@@ -125,19 +91,15 @@ int main(int argc, char ** argv) {
         switch(opt) {
             case 'h':
                 host = optarg;
-                //std::cout << "host: " << host << std::endl;
                 break;
             case 'p':
                 port = optarg;
-                //std::cout << "port: " << port << std::endl;
                 break;
             case 'd':
                 working_directory = optarg;
-                //std::cout << "directory: " << directory << std::endl;
                 break;
             case 't':
                 test = true;
-                //std::cout << "directory: " << directory << std::endl;
                 break;
         }
     }
@@ -147,13 +109,7 @@ int main(int argc, char ** argv) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-    // int optVal = 1;
-    // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &optVal, sizeof(optVal))) {
-    //     perror("setsockopt");
-    //     exit(EXIT_FAILURE);
-    // }
     int p = atoi(port.c_str());
-    //char host[sizeof(struct in_addr)];
     in_addr h;
     inet_pton(AF_INET, host.c_str(), &h);
     sockaddr_in addr;
@@ -173,25 +129,9 @@ int main(int argc, char ** argv) {
     while(running) {
         int connection = accept(server_fd, (struct sockaddr*)(&addr), &socklen);
         if (connection != -1) {
-            //std::thread worker(WorkConnection, connection, directory, test);
-            //worker.detach();
-            pthread_t slavethread;
-            int pthreadCheck = pthread_create(&slavethread, NULL, WorkConnection, &connection);
-            if(pthreadCheck != 0)
-            {
-                //std::cout << "Something wrong with pthread creation" << std::endl;
-            }
-            int pthrDet = pthread_detach(slavethread);
-            if(pthrDet != 0)
-            {
-                //std::cout << "Something wrong with pthread detaching" << std::endl;
-            }
+            std::thread worker(WorkConnection, connection);
+            worker.detach();
         }
     }
-    // valread = read(new_socket, buffer, 1024);
-    // printf("%s\n", buffer);
-    // send(new_socket, hello, strlen(hello), 0);
-    // printf("Hello message sent\n");
-    //shutdown(server_fd, SHUT_RDWR);
     return 0;
 }
